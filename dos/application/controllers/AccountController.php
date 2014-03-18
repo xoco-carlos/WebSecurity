a180e082ddc77b4dbe9d04c7cd189bb89cad3dad
xoco <xoco@websecurity>
2014-03-18 14:08:49 -0600

    Otra pruba de Zend Framework

---
+++ dos/application/controllers/AccountController.php
@@ -0,0 +1,34 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+    public function indexAction()
+    {
+        // action body
+    }
+    public function successAction()
+    {
+		if($this->_request->isPost()){
+			$username=$this->_request->getPost('username');
+			$password=$this->_request->getPost('password');
+			$email=$this->_request->getPost('email');
+			$vistas=$this->_request->getPost('vistas');
+			$tipo=$this->_request->getPost('tipo');
+			
+			//Initiate the SaveAccount model.
+			require_once "Registro.php";
+			$SaveAccount = Registro();
+			$SaveAccount->registro($username, $password, $email, $vistas, $tipo);
+		}else{
+			throw new Exception("Error 404 D:");
+		}
+    }
+    public function newAction()
+    {
+        // action body
+    }
+}
