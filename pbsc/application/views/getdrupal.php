43df53840037b51a4dc49cf48f9a6f6dc3b161c5
rcarmona <andres_zlatan@hotmail.com>
2014-03-21 21:14:03 -0600

    Getdrupal como clase
    Se creo una clase que se encarga de obtener la información de un nodo
    solicitado.

--- pbsc/application/views/getdrupal.php
+++ pbsc/application/views/getdrupal.php
@@ -1,94 +1,79 @@
 <?php
-/**
- * Get the articles as json format.
- * @param $uri the URI to get
- * @return array
- */
-function getArticles() {
-    // Get the arrays with all the articles.
-    $uri = "http://drupal.xoco.in/drupal7/?q=json";
-    $response = file_get_contents($uri); 
-    //$prueba=json_decode($response,TRUE);
-    // This will return an array, if you want an object, use json_decode($response) directly. 
-    return json_decode($response,TRUE);//$prueba;
-}
 
-function getTitulo($nodo){
-	$titulo = "";
-	$articulos = getArticles();
-	$noArt = count($articulos);
-	for ($i = 0 ;$i < $noArt; $i++){
-		if ($articulos[$i]['Nid']==$nodo){
-			$titulo=$articulos[$i]['title'];
-			break;
-		}
+class Articulos{
+	public function getArticles() {
+	    // Get the arrays with all the articles.
+	    $uri = "http://drupal.xoco.in/drupal7/?q=json";
+	    $response = file_get_contents($uri); 
+	    //$prueba=json_decode($response,TRUE);
+	    // This will return an array, if you want an object, use json_decode($response) directly. 
+	    return json_decode($response,TRUE);//$prueba;
 	}
-	return $titulo;
-}
-function getContenido($nodo){
-	$contenido = "";
-	$articulos = getArticles();
-	$noArt = count($articulos);
-	for ($i = 0 ;$i < $noArt; $i++){
-		if ($articulos[$i]['Nid']==$nodo){
-			$contenido=$articulos[$i]['contenido'];
-			break;
-		}
-	}
-	return $contenido;
-}
-function getFecha($nodo){
-	$fecha = "";
-	$articulos = getArticles();
-	$noArt = count($articulos);
-	for ($i = 0 ;$i < $noArt; $i++){
-		if ($articulos[$i]['Nid']==$nodo){
-			$fecha=$articulos[$i]['Post date'];
-			break;
-		}
-	}
-	return $fecha;
-}
 
-function getAutor($nodo){
-	$autor = "";
-	$articulos = getArticles();
-	$noArt = count($articulos);
-	for ($i = 0 ;$i < $noArt; $i++){
-		if ($articulos[$i]['Nid']==$nodo){
-			$autor=$articulos[$i]['autor'];
-			break;
+	public function getTitulo($nodo){
+		$titulo = "";
+		$articulos = $this->getArticles();
+		$noArt = count($articulos);
+		for ($i = 0 ;$i < $noArt; $i++){
+			if ($articulos[$i]['Nid']==$nodo){
+				$titulo=$articulos[$i]['title'];
+				break;
+			}
 		}
+		return $titulo;
 	}
-	return $autor;
-}
-
-function getImagen($nodo){
-	$imagen = "";
-	$articulos = getArticles();
-	$noArt = count($articulos);
-	for ($i = 0 ;$i < $noArt; $i++){
-		if ($articulos[$i]['Nid']==$nodo){
-			$imagen=$articulos[$i]['Image'];
-			break;
+	
+	public function getContenido($nodo){
+		$contenido = "";
+		$articulos = $this->getArticles();
+		$noArt = count($articulos);
+		for ($i = 0 ;$i < $noArt; $i++){
+			if ($articulos[$i]['Nid']==$nodo){
+				$contenido=$articulos[$i]['contenido'];
+				break;
+			}
 		}
+		return $contenido;
 	}
-	return $imagen;
-}
+	
+	public function getFecha($nodo){
+		$fecha = "";
+		$articulos = $this->getArticles();
+		$noArt = count($articulos);
+		for ($i = 0 ;$i < $noArt; $i++){
+			if ($articulos[$i]['Nid']==$nodo){
+				$fecha=$articulos[$i]['Post date'];
+				break;
+			}
+		}
+		return $fecha;
+	}
 
+	public function getAutor($nodo){
+		$autor = "";
+		$articulos = $this->getArticles();
+		$noArt = count($articulos);
+		for ($i = 0 ;$i < $noArt; $i++){
+			if ($articulos[$i]['Nid']==$nodo){
+				$autor=$articulos[$i]['autor'];
+				break;
+			}
+		}
+		return $autor;
+	}
+
+	public function getImagen($nodo){
+		$imagen = "";
+		$articulos = $this->getArticles();
+		$noArt = count($articulos);
+		for ($i = 0 ;$i < $noArt; $i++){
+			if ($articulos[$i]['Nid']==$nodo){
+				$imagen=$articulos[$i]['Image'];
+				break;
+			}
+		}
+		return $imagen;
+	}
+
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        <H1><?php echo getTitulo(1); ?></H1>
-		<H3><?php echo getFecha(1); ?></H3>
-		<H3><?php echo getAutor(1); ?></H3>
-		<p><?php echo getContenido(1); ?></p>
-		<img src=' <?php echo $getImagen(1); ?>'/>
-    </body>
-</html>
