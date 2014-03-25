0cab038c10720503e6a55882f3ad65c14365c9a3
xoco-carlos <xoco@d1stkfactory>
2014-03-25 21:28:43 +0000

    CGI sin comentarios

--- cgi/conexion.h
+++ cgi/conexion.h
@@ -21,59 +21,26 @@ class conn{
     void connection_users(char *us,char *pas);
     void connection_close();
 };
-  //private
   void conn::conection_sql(){
-  /*   char  nombre[60] = "root";
-     char  pass[60] = "becarios";
-     char  db [60] = "proyecto";
-printf("entro");
-
-     if(!strcmp(pass,"false") == 1){
-       strcpy(pass,"");
-     }else{
-       strcpy(pass,pass);
-     }
-     */
-     //cout << "Ingresa el nombre de tu base de datos: ";
-    // cin >> db;
-
      mysql_init( &mysql );
-    // cout << "entro de nuevooooo"; 
      if(mysql_real_connect(&mysql,"localhost",objectBD.GET_usBD(),objectBD.GET_psBD(),objectBD.GET_naBD(),0,NULL,0)){
-      // cout<<"conexion establecida"<<endl;
-       //select of db
-  //     connection_users();
      }else{
        cout<<"conexion fallida, verifique los datos"<<endl;
-       //conection_sql();
      }
-
-
-
   }
-  //public
   void conn::GET_conection_sql(){
-//printf("priemro\n");
     conection_sql();
-//printf("segundo\n");
   }
-  //public
   void conn::GET_time(){
     time_t now = time(0);
     char *tiempo = ctime(&now);
-    //cout<<"la hora es:"<<tiempo;
   }
-  //private
   void conn::connection_close(){
     mysql_close(&mysql);
-    //cout<<"conexion cerrada";
   }
-  //public
   void conn::close(){
-
     connection_close();
   }
-  //private
   void conn::connection_users(char *us,char *pas){
     int flag=0;
     char cadena1[30] = "/uwww/";
@@ -84,65 +51,34 @@ printf("entro");
     strcat(s1,"'");
     string str1 = s1;
     const char* string1 = str1.c_str();
-   // string str2 = str1;
-//    str2 += pas;
-//    const char* string1 = str1.c_str();
-//printf("%s",string1);
-  //  const char *query ="select id,name,password,tip_user from users";
     mysql_real_query(&mysql,string1,strlen(string1));
     MYSQL_RES *Result = mysql_store_result(&mysql);
     MYSQL_ROW row;
-    //ofstream data;
-    //data.open("usuarios.txt");
-//	cout << mysql_num_rows(Result) << "Numerosss";
     while(row = mysql_fetch_row(Result)){
-  //    cout << us <<" " << pas << " "<<row[1]<<" "<<row[2]<<endl;
+	cout<<"Location: http://web.xoco.in/front/controllers/loginController.php?name="<<row[1];
 	printf("<body style=\"background:#80BFFF\">");
 
 	if( mysql_num_rows(Result)!=0 ){
 		cout << "<h1> Bienvenido " << row[1]<< "</h1>";
 		strcat(cadena1,row[1]);		
 		ofstream fs(cadena1); 
-
-   // Enviamos una cadena al fichero de salida:
 	   	fs << row[0]<<"\n"<< row[2] << endl;
-   // Cerrar el fichero, 
-   // para luego poder abrirlo para lectura:
    		fs.close();		 
 		
 		flag=1;		
-		printf("<script> window.location.href = \"http:%c%cweb.xoco.in/front/controllers/loginController.php?name=%s\";</script>",47,47,row[1]);
+//		printf("<script> window.location.href = \"http:%c%cweb.xoco.in/front/controllers/loginController.php?name=%s\";</script>",47,47,row[1]);
 
-		/*printf("<button>");
-	  	
-		 printf("<A HREF=\"http:%c%cweb.xoco.in%ccgi%cses.php%cid=%s\">Continuar<%cA>",47,47,47,47,63,row[0],47);
-		printf("</button>");*/
-		//printf("<FORM METHOD=POST ACTION=\"../front/controllers/loginController.php\">");
-		//printf("<input type=\"hidden\" name=\"id\" value=\"%s\">",row[0]);
-		//printf("<input type=\"hidden\" name=\"name\" value=\"%s\">",row[1]);
-		//printf("<INPUT type=\"submit\" value=\"Enviar\">   </FORM>   ");
-		//std::cout << row[0];
-		//fprintf(stdout, "Afuerzaz");
 			}
 	
 
 
     }
 	if(flag==0){
-	
-	printf("<h1>Usuario o password invalidos.</h1>");
-//	printf("%s",string1);
-	//printf("<button>");
-	//printf("<A HREF=\"http:%c%cweb.xoco.in%ccgi\">Regresar<%cA>",47,47,47,47,63,47);
-	printf("<a href=\"http:%c%cweb.xoco.in%cfront%cloginView.php\">Regresar<%ca>",47,47,47,47,47,63,47);
-	//printf("</button>");
-
+		printf("<h1>Usuario o password invalidos.</h1>");
+		printf("<a href=\"http:%c%cweb.xoco.in%cfront%cloginView.php\">Regresar<%ca>",47,47,47,47,47,63,47);
+	}
+	printf("</body>");
 }
-printf("</body>");
-   // data.close();
-  }
-//public
- void conn::slect(char *us,char *pas){
-	
-        connection_users(us,pas);
-        }
+void conn::slect(char *us,char *pas){
+	connection_users(us,pas);
+}
--- cgi/pcon.cpp
+++ cgi/pcon.cpp
@@ -18,99 +18,56 @@ int main(){
   char *us,*sus; 
   char *ps,*sps;
   int flag=0;
+	printf("Content-Type: text/html\n\n");
+ 	info=(char*)malloc(sizeof(char)*1000);
+ 	cadena=getenv("CONTENT_LENGTH");
+ 	longitud=strtod(cadena,NULL);
+	
+	for(i=0;i<longitud;i++){ 
+		fscanf(stdin,"%c",&info[i]);  
+	}
 
-//system("tex");
-  // ofstream fs("/uwww/nsss.txt"); 
-
-   // Enviamos una cadena al fichero de salida:
-  // fs << "Hola, mundo" << endl;
-   // Cerrar el fichero, 
-   // para luego poder abrirlo para lectura:
-//   fs.close();
-
-//printf("Content-Type: application/x-httpd-php");
-printf("Content-Type: text/html\n\n");
-//printf("<head> ");
-//printf("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> "); 
-//printf("</head>"); 
- 
- info=(char*)malloc(sizeof(char)*1000);
- cadena=getenv("CONTENT_LENGTH");
- longitud=strtod(cadena,NULL);
-
- for(i=0;i<longitud;i++)
- { 
-  fscanf(stdin,"%c",&info[i]);  
-    
-  }
-
-  i=longitud;
-  info[i]='\0';
-  //printf("%d\n%s",i,info);
-
-/// 
- token = strtok(info , "&");  
- us=token;      
-
-  while (token != NULL){
-                
-                token = strtok(NULL,"&");
+	i=longitud;
+	info[i]='\0';
+	token = strtok(info , "&");  
+	us=token;      
+	while (token != NULL){
+		token = strtok(NULL,"&");
 		ps=token;
 		break;
-        }
-if(strlen(us)>5 && strlen(ps)>5){
-//printf("%s\n",ps);
-token = strtok(us , "="); 
-sus=token;
- while (token != NULL){
-                
-                token = strtok(NULL,"=");
-                us=token;
-                break;
-        }
-//printf("%s\n",us);
+	}
+	if(strlen(us)>5 && strlen(ps)>5){
+		token = strtok(us , "="); 
+		sus=token;
+		while (token != NULL){
+			token = strtok(NULL,"=");
+			us=token;
+			break;
+		}
+		token = strtok(ps , "=");  
+		sps=token;
+ 		while (us != NULL){
+			token = strtok(NULL,"=");
+			ps=token;
+			break;
+		}
+		if((strchr( us, '%' ))!=NULL || (strchr( us, '\'' ))!=NULL || (strchr( ps, '\'' ))!=NULL ){
+			flag=1;
+		}
+	}
+	else{
+		flag=1;
+	}
+	object.GET_conection_sql();
+	object.GET_time();
 
-
-token = strtok(ps , "=");  
-sps=token;
- while (us != NULL){
-                
-
-                token = strtok(NULL,"=");
-                ps=token;
-                break;
-        }
-//printf("%s\n",ps);
-
-if((strchr( us, '%' ))!=NULL || (strchr( us, '\'' ))!=NULL || (strchr( ps, '\'' ))!=NULL ){
-flag=1;
-}
-
-
-}
-else{
-flag=1;
-}
-  object.GET_conection_sql();
-//printf("ssssnes\n");
-  object.GET_time();
-
-if((flag==0 && strcmp(sus,"user")==0)&&(strcmp(sps,"pass")==0)&& us!=NULL && ps!=NULL){
-   object.slect(us,ps);
+	if((flag==0 && strcmp(sus,"user")==0)&&(strcmp(sps,"pass")==0)&& us!=NULL && ps!=NULL){
+		object.slect(us,ps);
    }
-
-else{
- printf("<h1>No has ingresado datos en usuario o en password.</h1>");
- //printf("<button>");
- //printf(" <A HREF=\"http:%c%cweb.xoco.in%ccgi\">Regresar<%cA>",47,47,47,47);
- printf("<a href=\"http:%c%cweb.xoco.in%cfront%cloginView.php\">Regresar<%ca>",47,47,47,47,47);
- //printf("</button>");
-
-}
+	else{
+		printf("<h1>No has ingresado datos en usuario o en password.</h1>");
+ 		printf("<a href=\"http:%c%cweb.xoco.in%cfront%cloginView.php\">Regresar<%ca>",47,47,47,47,47);
+	}
   object.close();
   
 }
-
-
-
-
