2129cb267ff5801976f3d550586054e066b3903b
jhernandezp <josecarmen@comunidad.unam.mx>
2014-03-24 21:57:38 -0600

    base.h Valores para la conexion a la BD

---
+++ cgi/base.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <mysql.h>
+#include <string.h>
+#include <ctime>
+#include <fstream>
+#include <cstring>
+using std::string;
+
+class getBD{
+
+  public:
+    const char *GET_usBD();
+    const char *GET_psBD();
+    const char *GET_naBD();
+
+};
+
+const char  *getBD::GET_usBD(){
+
+        const char  *c="root";
+        return c;
+
+        }
+
+const char  *getBD::GET_psBD(){
+
+
+const char  *c="becarios";
+return c;
+
+
+        }
+
+const char  *getBD::GET_naBD(){
+
+
+
+const char  *c="proyecto";
+return c;
+
+
+        }
+
