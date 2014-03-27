cf149cc9687dcc1b8a2f16a08faa25e70b56716a
xoco-carlos <xoco@d1stkfactory>
2014-03-27 04:42:05 +0000

    Modificaciones para ADD

--- php/addUserView.php
+++ php/addUserView.php
@@ -7,17 +7,17 @@
 		<form method="post" action="controllers/addUserController.php">
 			<h2>Registro</h2>
 			<p>Registrar nuevo usuario</p></br>
-			<label class="description" for="element_1">Username </label>
-			<input name="username" class="element text medium" type="text" maxlength="255" value="" required/> </br>
-			<label class="description" for="element_2">Password </label>
-			<input name="password" class="element text medium" type="password" maxlength="255" value="" required/> </br>
-			<label class="description" for="element_3">Confirmar password </label>
-			<input name="password2" class="element text medium" type="password" maxlength="255" value="" required/> </br>
-			<label class="description" for="element_4">E-mail </label>
-			<input name="email" class="element text medium" type="email" maxlength="255" value="" required/> </br>
-			<label class="description" for="element_6">Admin </label>
-			<input name="tipo" class="element text medium" type="checkbox"/>
-			<input class="button_text" type="submit" name="submit" value="Submit" />
+			<label >Username </label>
+			<input name="username" type="text" maxlength="255" required/> </br>
+			<label >Password </label>
+			<input name="password" type="password" maxlength="255" required/> </br>
+			<label >Confirmar password </label>
+			<input name="password2" type="password" maxlength="255" required/> </br>
+			<label >E-mail </label>
+			<input name="mail" type="email" maxlength="255" required/> </br>
+			<label >Admin </label>
+			<input name="type" type="checkbox"/>
+			<input type="submit" name="submit" value="Submit" />
 		</form>
 	</body>
 </html>
--- php/controllers/addUserController.php
+++ php/controllers/addUserController.php
@@ -4,48 +4,66 @@
 	include_once dirname(__FILE__).('/../includes/alerts.php');
 	include_once dirname(__FILE__).('/../includes/checks.php');
 	$numero=isLogged();
-	if($numero < 0){
+	if($numero==0){
 		message("error","Go home",'/front/loginView.php');
+		die();
+	}
+	if($password!=$password2){
+		message("error","Passwords doesn't match",'/front/addUserView.php');
 		die();
 	}
 	$username	= filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 	$password	= filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 	$password2	= filter_input(INPUT_POST, 'password2',FILTER_SANITIZE_STRING);
-	$email		= filter_input(INPUT_POST, 'email', 	FILTER_SANITIZE_STRING);
-	$type			= filter_input(INPUT_POST, 'type', 		FILTER_SANITIZE_STRING);
-	$type=='on'?$type=1:$type=0;
-	if($password!=$password2){
-		message("error","Passwords doesn't match",'/front/addUserView.php');
-		die();
-	}
+	$type			= filter_input(INPUT_POST, 'type', FILTER_VALIDATE_BOOLEAN);
+	$email		= filter_input(INPUT_POST, 'email',FILTER_SANITIZE_EMAIL);
+#	$type=='on'?$type=1:$type=0;
 	if($username=='' || $password=='' || $email='' ){
 		message("error","Fill all fields",'/front/addUserView.php');
 		die();
 	}
+#	message("error",$username.' '.$password.' '.$email.' '.$type,'/front/addUserView.php');
+/*
+	echo "Name: {$username}";
+	echo "Pass: {$password}";
+	echo "mail {$email}";
+	echo "type: {$type}";*/
 	$user=new User();
    $view=new View();
 	if($user->isSign($username)==1){
 		$user->setName($username);
+		echo $username;
+		echo '</br>';
 		$user->setPassword($password);
+		echo $password;
+		echo '</br>';
 		$user->setEmail($email);
+		echo $email;
+		echo '</br>';
 		$user->setTipo($type);
+		echo $type;
+		echo '</br>';
 		$lastID=$user->insert();
+		echo $lastID;
+		echo '</br>';
 		$view->setUserID($lastID);
 		$order=1;
 		foreach($view->getAdminViews() as $value){
 			$view->setOrder($order);
 			$view->setViewID($value);
-			$view->insert();
-			$order++;
+			echo $view->insert();
+			echo $order++;
 		}
 		$view->getDB()->CloseConnection();
 		$user->getDB()->CloseConnection();
-		message("success","User created successfully","/front/");
+		echo "Se agrego el usuario";
+	#	message("success","User created successfully","/front/");
 		#header('Location:'.$_SERVER["HTTP_REFERER"].'?mensaje=User '.$username.' created successfully');
    }
 	else{
-		message("error","User already exists","/front/loginView.php");
-		die();
+		echo "El usuario ya existe";
+	#	message("error","User already exists","/front/loginView.php");
+	#	die();
 		#header('Location:'.$_SERVER["HTTP_REFERER"].'?mensaje=User '.$username.' already exists');
 	}
 ?>
