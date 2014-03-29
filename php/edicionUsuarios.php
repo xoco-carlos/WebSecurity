ebe2743385ee542cbde66da7a07d3c42b3f29e70
rcarmona <andres_zlatan@hotmail.com>
2014-03-28 22:34:38 -0600

    Edicion Usuario
    Comentarios

--- php/edicionUsuarios.php
+++ php/edicionUsuarios.php
@@ -1,3 +1,9 @@
+<!--
+Carmona Domínguez Ricardo Andrés
+Archivo dedicado a mostrar una lista con los nombres de usuarios registrados, dando 
+la opcion de modificar sus datos (nombre y mail), su contraseña o eliminarlos.
+
+-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,15 +19,17 @@
         </div>
         </br></br></br>
 		<?php 
+		//Incluimos los archivos necesarios
 			include_once("controllers/showUserController.php");
 			include_once('includes/checks.php');
 			include_once('includes/alerts.php');
 			include_once('controllers/setNodeController.php');
-			$numero=isLogged();
-			if($numero==0){
+			$numero=isLogged(); //Verificamos si quien visita la pagina es un usuario con una sesión activa
+			if($numero==0){ 
 				message('error','You must login to edit views','/front/loginView.php');
 				die();
         }
+			//Si el usuario tiene una sesion activa, mostramos la lista de usuarios
 			printUsers();
 		?>
 	</body>
