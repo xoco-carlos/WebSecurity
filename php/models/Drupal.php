d08adcac2daabc162ed6480c6cead40b10daf0cc
rcarmona <andres_zlatan@hotmail.com>
2014-03-28 23:27:44 -0600

    Drupal
    Comentarios

--- php/models/Drupal.php
+++ php/models/Drupal.php
@@ -1,6 +1,12 @@
 <?php
-
+/*
+* Autor: Richard
+* Colaboradores:
+* Funciones que se conectan al servidor de drupal para hacer un vaciado de su contenido y obtener
+  datos especificos de un nodo indicado.
+*/
 class Drupal{
+	//Obtiene un arreglo de los articulos publicados en drupal
 	public function getArticles() {
 	    // Get the arrays with all the articles.
 	    $uri = "http://drupal.xoco.in/drupal7/?q=json";
@@ -10,6 +16,7 @@ class Drupal{
 	    return json_decode($response,TRUE);//$prueba;
 	}
 
+	//Funciones que regresan información especifica del nodo indicado
 	public function getTitulo($nodo){
 		$titulo = "";
 		$articulos = $this->getArticles();
